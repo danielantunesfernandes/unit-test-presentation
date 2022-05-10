08a7554d4300cfd56524cb3b4adb29c53bd2bb5b
Daniel Fernandes <daniel.fernandes@diconum.com>
2022-05-10 14:00:35 +0100

    refactor unit tests

--- src/Incrementor/Incrementor.test.tsx
+++ src/Incrementor/Incrementor.test.tsx
@@ -13,8 +13,8 @@ describe("Incrementor.tsx", () => {
     render(<Incrementor />);
     const keyTextElem = screen.getByText("Incrementor");
     expect(keyTextElem).toBeInTheDocument();
-    const inputElem = screen.getByTestId("input");
-    expect(inputElem).toBeInTheDocument();
+    const elem = screen.getByTestId("result");
+    expect(elem).toBeInTheDocument();
     const buttonIncrement = screen.getByText("Increment");
     expect(buttonIncrement).toBeInTheDocument();
 
@@ -24,41 +24,41 @@ describe("Incrementor.tsx", () => {
 
   test("When component is rendered should have as default value 0", () => {
     render(<Incrementor />);
-    const inputElem = screen.getByTestId("input");
-    expect(inputElem).toBeInTheDocument();
-    expect(inputElem).toHaveAttribute("value", "0");
+    const elem = screen.getByTestId("result");
+    expect(elem).toBeInTheDocument();
+    expect(elem.innerHTML).toBe("0");
   });
 
   test("When push the button the value should be incremented", () => {
     render(<Incrementor />);
-    const inputElem = screen.getByTestId("input");
-    expect(inputElem).toBeInTheDocument();
-    expect(inputElem).toHaveAttribute("value", "0");
+    const elem = screen.getByTestId("result");
+    expect(elem).toBeInTheDocument();
+    expect(elem.innerHTML).toBe("0");
 
     const button = screen.getByText("Increment");
     expect(button).toBeInTheDocument();
     fireEvent.click(button);
-    expect(inputElem).toHaveAttribute("value", "1");
+    expect(elem.innerHTML).toBe("1");
 
     fireEvent.click(button);
-    expect(inputElem).toHaveAttribute("value", "2");
+    expect(elem.innerHTML).toBe("2");
   });
 
   test("When the user push the button to calculate the power of two should the input value be updated with the power of two of the value", () => {
     mockCalcPowerTwo.mockReturnValue(4);
 
     render(<Incrementor />);
-    const inputElem = screen.getByTestId("input");
-    expect(inputElem).toBeInTheDocument();
-    expect(inputElem).toHaveAttribute("value", "0");
+    const elem = screen.getByTestId("result");
+    expect(elem).toBeInTheDocument();
+    expect(elem.innerHTML).toBe("0");
 
     const button = screen.getByText("Increment");
     expect(button).toBeInTheDocument();
     fireEvent.click(button);
-    expect(inputElem).toHaveAttribute("value", "1");
+    expect(elem.innerHTML).toBe("1");
 
     fireEvent.click(button);
-    expect(inputElem).toHaveAttribute("value", "2");
+    expect(elem.innerHTML).toBe("2");
 
     const buttonCalc = screen.getByText("Power of two");
     expect(buttonCalc).toBeInTheDocument();
@@ -67,6 +67,6 @@ describe("Incrementor.tsx", () => {
 
     expect(mockCalcPowerTwo).toBeCalled();
     expect(mockCalcPowerTwo).toBeCalledWith(2);
-    expect(inputElem).toHaveAttribute("value", "4");
+    expect(elem.innerHTML).toBe("4");
   });
 });
--- src/Incrementor/Incrementor.tsx
+++ src/Incrementor/Incrementor.tsx
@@ -3,18 +3,22 @@ import useCalc from "../hooks/useCalc";
 function Incrementor(): JSX.Element {
   const [numberValue, setNumberValue] = useState(0);
   const { calcPowerTwo } = useCalc();
+
   const incrementValue = () => {
     const result = numberValue + 1;
     setNumberValue(result);
   };
+
   const calcPowerTwoValue = () => {
     const result: number = calcPowerTwo(numberValue);
     setNumberValue(result);
   };
+
   return (
     <div>
       <h1>Incrementor</h1>
-      <input type="text" data-testid="input" defaultValue={numberValue} />
+      Value: <span data-testid="result">{numberValue}</span>
+      <br />
       <button onClick={incrementValue}>Increment</button>
       <button onClick={calcPowerTwoValue}>Power of two</button>
     </div>
